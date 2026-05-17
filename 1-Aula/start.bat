0c43741f286b3c3cc0ae84e571ef4a01100e4845
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2026-05-16 21:04:16 -0300

    Update start.bat

--- 1-Aula/start.bat
+++ 1-Aula/start.bat
@@ -1,3 +1,3 @@
 @echo off
-start "" cs2.exe -insecure -condebug -port 27017 -dedicated -console -usercon +map de_dust2 +game_type 0 +game_mode 1 +sv_competitive_official_5v5 1 -maxplayers_override 10 -maxplayers 14 +sv_setsteamaccount SUAKEY +hostname "Server Tutorial [#1] - MIX"
+start "" cs2.exe -condebug -port 27017 -dedicated -console -usercon +map de_dust2 +game_type 0 +game_mode 1 +sv_competitive_official_5v5 1 -maxplayers_override 10 -maxplayers 14 +sv_setsteamaccount SUAKEY +hostname "PumBa Cs2 Dedicated Server [#1] - MIX"
 exit
